4a302dd63df6f36d4a63584a43fe37c34bf4bb76
singlamohit1 <singlamohit98@gmail.com>
2022-07-25 21:52:05 +0530

    refactored code

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-import swaggerJsdoc from "swagger-jsdoc";
+// import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import dotenv from "dotenv";
 import { initDb } from "./config/db/index.js";
+// import {swaggerDocument} from './swagger.json';
+import { createRequire } from "module"; // Bring in the ability to create the 'require' method
+const require = createRequire(import.meta.url); // construct the require method
+const swaggerDocument = require("./swagger.json");
+
 import {
   cumulativeReturnsRouter,
   homeRouter,
@@ -26,30 +31,32 @@ app.use("/trade", tradeRouter);
 app.use("/portfolio", portfolioRouter);
 app.use("/returns", cumulativeReturnsRouter);
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Trading App",
-      version: "1.0.0",
-      description: "This is a trading application",
-      contact: {
-        name: "Mohit Singla",
-        email: "singlamohit98@gmail.com",
-      },
-    },
-    servers: [
-      {
-        url: process.env.SERVERURL,
-      },
-    ],
-  },
-  apis: ["./src/app.js"],
-};
+// const options = {
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "Trading App",
+//       version: "1.0.0",
+//       description: "This is a trading application",
+//       contact: {
+//         name: "Mohit Singla",
+//         email: "singlamohit98@gmail.com",
+//       },
+//     },
+//     servers: [
+//       {
+//         url: process.env.SERVERURL,
+//       },
+//     ],
+//   },
+//   apis: ["./routes/returns.js"],
+// };
 
-const specs = swaggerJsdoc(options);
+// const specs = swaggerJsdoc(options);
 
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.listen(process.env.PORT || 8000, () => {
   console.log(`App listening on port`, process.env.PORT || 8000);
